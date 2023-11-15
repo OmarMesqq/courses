3aa55eccde6a45f7bb42e28a4803de4187a788a3
OmarMesqq <omarmsqt@gmail.com>
2023-11-15 19:38:11 -0300

    refactor: :recycle: use type alias for short int and add remaining couts

--- cpp_essentials/library_card.cpp
+++ cpp_essentials/library_card.cpp
@@ -1,6 +1,8 @@
 #include <format>
 #include <iostream>
 
+using sint = short int;
+
 struct LibraryCard { 
 	const char* title{};
 	const char* author{};
@@ -8,28 +10,38 @@ struct LibraryCard {
 	const char* subject{};
 	const char* isbn{};
 	const char* oclc{};
-	int ddc1{};
-        int ddc2{};	
-	int yearPublished{}; 
-	int yearAcquired{};
-	int quantity{};
+	sint ddc1{};
+    sint ddc2{};	
+	sint yearPublished{}; 
+	sint yearAcquired{};
+	sint quantity{};
 };
 
 int main() {
 	LibraryCard lordOfTheFlies {
-	"Lord of the Flies", 
-	"William Golding",
-	"Penguin",
-	"Fiction", 
-	"2989018429-247189047209", 
-	"12555530", 
-	12402453,
-	243,
-        1999,
-	2000,
-	2,	
+		"Lord of the Flies", 
+		"William Golding",
+		"Penguin Books",
+		"Fiction", 
+		"978-0399501487", 
+		"47677622", 
+		823,
+		00,
+    	1954,
+		2023,
+		1,	
 	};
-	std::cout << std::format("Title: {}", lordOfTheFlies.title);
-
+	
+	std::cout << std::format("Title: {}\n", lordOfTheFlies.title);
+	std::cout << std::format("Author: {}\n", lordOfTheFlies.author);
+	std::cout << std::format("Publisher: {}\n", lordOfTheFlies.publisher);
+	std::cout << std::format("Subject: {}\n", lordOfTheFlies.subject);
+	std::cout << std::format("ISBN: {}\n", lordOfTheFlies.isbn);
+	std::cout << std::format("OCLC: {}\n", lordOfTheFlies.oclc);
+	std::cout << std::format("Dewey decimal: {}.{}\n", lordOfTheFlies.ddc1, lordOfTheFlies.ddc2);
+	std::cout << std::format("Year published: {}\n", lordOfTheFlies.yearPublished);
+	std::cout << std::format("Year acquired: {}\n", lordOfTheFlies.yearAcquired);
+	std::cout << std::format("Quantity: {}\n", lordOfTheFlies.quantity);
+	
 	return 0; 
 }
