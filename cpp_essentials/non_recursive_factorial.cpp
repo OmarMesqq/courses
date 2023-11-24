7e31c7acc70be357b05086fa3f1e08f71b1b5789
OmarMesqq <omarmsqt@gmail.com>
2023-11-24 15:31:10 -0300

    feat: :sparkles: non recursive factorial

---
+++ cpp_essentials/non_recursive_factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int non_recursive_factorial(const int n) {
+    if (n <= 1) {
+        if (n < 0) {
+            throw std::runtime_error("Factorial not defined for negative numbers");
+        }
+        return 1;
+    }
+    int product = 1;
+
+    for (int i = 1; i<= n; i++) {
+        product *= i;
+    }
+    return product;
+}
+
+int main() {
+    std::cout << non_recursive_factorial(10) << std::endl;
+    return 0;
+}
