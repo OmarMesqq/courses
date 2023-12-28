cf2ffbc612f220700cd7bc4f8247538f58a318ac
OmarMesqq <omarmsqt@gmail.com>
2023-12-28 00:23:11 -0300

    refactor: :recycle: trim some more comments

--- cpp_essentials/file_transformation_stl.cpp
+++ cpp_essentials/file_transformation_stl.cpp
@@ -16,11 +16,7 @@ struct std::formatter<Frac<T>>: std::formatter<unsigned> {
     }
 };
 
-
-// This variadic template creates a print function
-// through metaprogramming and is marked as constant 
-// expression, meaning it will be evaluated in compile time
-// if given constant expressions as input  
+ 
 template<typename ... Args> 
 constexpr void print(const std::string_view str_fmt, Args&&... args) {
     fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
